4365ae907ec0b37013abf6658bf7c70c541c792d
Hai Nguyen <memyselfandhai@gmail.com>
2017-12-06 12:32:27 -0600

    post route added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const mongoose = require("mongoose");
+const mongooseeder = require("mongooseeder");
+const models = require("../models");
+const User = models.User;
+const Commentable = models.Commentable;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/newpost", function(req, res, next) {
+  console.log("post started");
+  User.find({ username: req.body.username }).then(result => {
+    // console.log(result[0]._id)
+
+    let newPost = {
+      userId: result[0]._id,
+      title: req.body.title,
+      text: req.body.text,
+      parentId: 0,
+      score: 0
+    };
+
+    Commentable.create(newPost);
+    res.send("respond with a resource");
+  });
+});
+
+router.get("/newpost", (req, res, next) => {
+  res.render("form");
 });
 
 module.exports = router;
